3862dedc84d912743b548f337b7c3e0d91dc080b
AdrikLarreategui <adrian15186@hotmail.com>
2023-12-11 14:04:09 +0100

    commit users route cambiado

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,37 @@
 const { User } = require('../models/index.js');
 
-const UserController = {
-  create(req, res) {
-    req.body.role = "user";
+// const UserController = {
+//   create(req, res) {
+//     req.body.role = "user";
 
-    User.create(req.body)
-      .then(user => res.status(201).send({ message: 'Usuario creado con éxito', user }))
-      .catch(error => {
-        console.error(error);
-        res.status(500).send('Error al crear usuario');
-      });
-  },
+//     User.create(req.body)
+//       .then(user => res.status(201).send({ message: 'Usuario creado con éxito', user }))
+//       .catch(error => {
+//         console.error(error);
+//         res.status(500).send('Error al crear usuario');
+//       });
+//   },
   //ADD MORE METHODS HERE
-  
+  const UserController = {
+    findAll(req, res) {
+        // Puedes personalizar la lógica según tus necesidades
+        User.findAll()
+          .then(User => res.status(200).json(User))
+          .catch(error => {
+            console.error(error);
+            res.status(500).send('Error al obtener relaciones');
+          });
+        },
+      create(req, res) {
+        req.body.role = "user";
+    
+        User.create(req.body)
+          .then(user => res.status(201).send({ message: 'Usuario creado con éxito', user }))
+          .catch(error => {
+            console.error(error);
+            res.status(500).send('Error al crear usuario');
+          });
+      },
 };
 
 module.exports = UserController;
