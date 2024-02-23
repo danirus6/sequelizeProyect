28f129b7f29304b765ea47b8f0c5b37983eca42a
danirus6 <danirus6@gmail.com>
2024-02-23 20:16:14 +0100

    update back

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const cors = require('cors');
+
+// Configuración simple de CORS para permitir peticiones desde 'http://localhost:5173'
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 // Middleware para parsear JSON
 app.use(express.json());
@@ -11,8 +17,7 @@ app.use('/users', require('./routes/userRoutes'));
 app.use('/products', require('./routes/productRoutes'));
 app.use('/orders', require('./routes/orderRoutes'));
 app.use('/categories', require('./routes/categoryRoutes'));
-// app.use('/productOrders', require('./routes/productOrderRoutes'));
 
 app.listen(PORT, () => {
-  console.log(`Servidor en ejecución en el puerto http://localhost:${PORT}`);
+  console.log(`Servidor en ejecución en el puerto http://localhost:${PORT}`);
 });
